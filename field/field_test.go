dd3ff4d450fa90a6ef6061cc8094c712ddf20d3b
Manjeet <mthandani@grepruby.com>
2018-07-25 14:35:21 +0530

    Added test cases for subtraction over field

--- field/field_test.go
+++ field/field_test.go
@@ -18,9 +18,29 @@ var additionTests = []testpair{
 	{[]UInt64{2305843009213693950, 1}, 0},
 }
 
+var subtractionTests = []testpair{
+	{[]UInt64{7, 5}, 2},
+	{[]UInt64{4, 8}, P - 4},
+	{[]UInt64{18446744073709551615, 18446744073709551615}, 0},
+	{[]UInt64{P, 5}, P - 5},
+}
+
 func TestAddition(t *testing.T) {
 	for _, pair := range additionTests {
 		v := NewField(pair.data[0]).Add(NewField(pair.data[1]))
+		if v != NewField(pair.res) {
+			t.Error(
+				"For", pair.data,
+				"expected", pair.res,
+				"got", v,
+			)
+		}
+	}
+}
+
+func TestSubtraction(t *testing.T) {
+	for _, pair := range subtractionTests {
+		v := NewField(pair.data[0]).Sub(NewField(pair.data[1]))
 		if v != NewField(pair.res) {
 			t.Error(
 				"For", pair.data,
