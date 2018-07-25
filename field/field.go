158ded4446f5dafa4165d23eb94ba8a31b8f60f0
Manjeet <mthandani@grepruby.com>
2018-07-25 20:51:47 +0530

    Implemented multiplication over field

--- field/field.go
+++ field/field.go
@@ -2,6 +2,8 @@ package field
 
 import (
 	"log"
+
+	"github.com/cznic/mathutil"
 )
 
 type UInt64 uint64
@@ -26,6 +28,15 @@ func (self UInt64) reduceOnceAssert() UInt64 {
 		log.Fatal("Error: Expected result should be less than field size")
 	}
 	return res
+}
+
+func (self UInt64) reduceOnceMul(op2 UInt64) UInt64 {
+	var value UInt64 = (self << 3) | (op2 >> 61)
+	value = (op2 & P) + value
+	if value == P {
+		return 0
+	}
+	return value
 }
 
 func asLimbs(x UInt64) (uint32, uint32) {
@@ -63,4 +74,17 @@ func (self Field) Sub(op2 Field) Field {
 // SubAssign works same as Sub, assigns final value to self
 func (self *Field) SubAssign(op2 Field) {
 	*self = self.Sub(op2)
+}
+
+// Mul muliplies two field elements and reduces the resulting number in the field
+func (self Field) Mul(op2 Field) Field {
+	var high, low uint64 = mathutil.MulUint128_64(uint64(self.fp), uint64(op2.fp))
+	var rh, rl = UInt64(high), UInt64(low)
+	var res = rh.reduceOnceMul(rl).reduceOnceAssert()
+	return Field{res}
+}
+
+// MulAssign works same as Mul, assigns final value to self
+func (self *Field) MulAssign(op2 Field) {
+	*self = self.Mul(op2)
 }
