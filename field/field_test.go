c20e147363686bdeb538799129409c1583e9e559
Manjeet <mthandani@grepruby.com>
2018-07-25 14:32:20 +0530

    Added test cases for addtion over field

---
+++ field/field_test.go
@@ -0,0 +1,32 @@
+package field
+
+import (
+	"testing"
+)
+
+type testpair struct {
+	data []UInt64
+	res  UInt64
+}
+
+var additionTests = []testpair{
+	{[]UInt64{7, 5}, 12},
+	{[]UInt64{P - 2, 5}, 3},
+	{[]UInt64{2193980333835211996, 621408416523297271}, 509545741144815316},
+	{[]UInt64{18446744073709551615, 18446744073709551615}, 14},
+	{[]UInt64{2305843009213693950, 3}, 2},
+	{[]UInt64{2305843009213693950, 1}, 0},
+}
+
+func TestAddition(t *testing.T) {
+	for _, pair := range additionTests {
+		v := NewField(pair.data[0]).Add(NewField(pair.data[1]))
+		if v != NewField(pair.res) {
+			t.Error(
+				"For", pair.data,
+				"expected", pair.res,
+				"got", v,
+			)
+		}
+	}
+}
