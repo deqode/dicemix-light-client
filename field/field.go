c8266be1716db3ec0a6abf3fe550ea0eabc56315
Manjeet <mthandani@grepruby.com>
2018-07-27 21:51:29 +0530

    Added some comments

--- field/field.go
+++ field/field.go
@@ -6,32 +6,37 @@ import (
 	"github.com/cznic/mathutil"
 )
 
+// UInt64 - GO basic object notation
+// to perform operations on basic types
 type UInt64 uint64
 
+// P - size of field
+// The field size -- 2305843009213693951
 const P UInt64 = (1 << 61) - 1
 
+// Field -- It is consistent iff 0 <= src.0 <= P.
 type Field struct {
 	fp UInt64
 }
 
-func (self UInt64) reduceOnce() UInt64 {
-	var value = (self & P) + (self >> 61)
+func (src UInt64) reduceOnce() UInt64 {
+	var value = (src & P) + (src >> 61)
 	if value == P {
 		return 0
 	}
 	return value
 }
 
-func (self UInt64) reduceOnceAssert() UInt64 {
-	var res UInt64 = self.reduceOnce()
+func (src UInt64) reduceOnceAssert() UInt64 {
+	var res = src.reduceOnce()
 	if res >= P {
 		log.Fatal("Error: Expected result should be less than field size")
 	}
 	return res
 }
 
-func (self UInt64) reduceOnceMul(op2 UInt64) UInt64 {
-	var value UInt64 = (self << 3) | (op2 >> 61)
+func (src UInt64) reduceOnceMul(op2 UInt64) UInt64 {
+	var value = (src << 3) | (op2 >> 61)
 	value = (op2 & P) + value
 	if value == P {
 		return 0
@@ -49,42 +54,42 @@ func NewField(value UInt64) Field {
 }
 
 // Neg negates number in the field
-func (self Field) Neg() Field {
-	return Field{(P - self.fp).reduceOnce().reduceOnceAssert()}
+func (src Field) Neg() Field {
+	return Field{(P - src.fp).reduceOnce().reduceOnceAssert()}
 }
 
 // Add adds two field elements and reduces the resulting number in the field
-func (self Field) Add(op2 Field) Field {
-	return Field{(self.fp + op2.fp).reduceOnce().reduceOnceAssert()}
+func (src Field) Add(op2 Field) Field {
+	return Field{(src.fp + op2.fp).reduceOnce().reduceOnceAssert()}
 }
 
-// AddAssign works same as Add, assigns final value to self
-func (self *Field) AddAssign(op2 Field) {
-	*self = self.Add(op2)
+// AddAssign works same as Add, assigns final value to src
+func (src *Field) AddAssign(op2 Field) {
+	*src = src.Add(op2)
 }
 
 // Sub subtracts two field elements and reduces the resulting number in the field
-func (self Field) Sub(op2 Field) Field {
-	if op2.fp > self.fp {
-		return Field{(P - op2.fp + self.fp).reduceOnce().reduceOnceAssert()}
+func (src Field) Sub(op2 Field) Field {
+	if op2.fp > src.fp {
+		return Field{(P - op2.fp + src.fp).reduceOnce().reduceOnceAssert()}
 	}
-	return Field{(self.fp - op2.fp).reduceOnce().reduceOnceAssert()}
+	return Field{(src.fp - op2.fp).reduceOnce().reduceOnceAssert()}
 }
 
-// SubAssign works same as Sub, assigns final value to self
-func (self *Field) SubAssign(op2 Field) {
-	*self = self.Sub(op2)
+// SubAssign works same as Sub, assigns final value to src
+func (src *Field) SubAssign(op2 Field) {
+	*src = src.Sub(op2)
 }
 
 // Mul muliplies two field elements and reduces the resulting number in the field
-func (self Field) Mul(op2 Field) Field {
-	var high, low uint64 = mathutil.MulUint128_64(uint64(self.fp), uint64(op2.fp))
+func (src Field) Mul(op2 Field) Field {
+	var high, low uint64 = mathutil.MulUint128_64(uint64(src.fp), uint64(op2.fp))
 	var rh, rl = UInt64(high), UInt64(low)
 	var res = rh.reduceOnceMul(rl).reduceOnceAssert()
 	return Field{res}
 }
 
-// MulAssign works same as Mul, assigns final value to self
-func (self *Field) MulAssign(op2 Field) {
-	*self = self.Mul(op2)
+// MulAssign works same as Mul, assigns final value to src
+func (src *Field) MulAssign(op2 Field) {
+	*src = src.Mul(op2)
 }
