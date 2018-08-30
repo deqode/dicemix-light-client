e24c095a1b43d0298ba38c3a7f553e3f48046132
Manjeet Thadani <mthandani@grepruby.com>
2018-08-30 15:58:43 +0530

    Added some comments

--- ecdsa/p256.go
+++ ecdsa/p256.go
@@ -19,20 +19,21 @@ func NewCurveECDSA() ECDSA {
 // GenerateKey generates a public/private key pair using entropy from rand.
 // If rand is nil, crypto/rand.Reader will be used.
 func (e *curveP256) GenerateKeyPair() ([]byte, []byte, error) {
+	// generates random privateKey
 	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 
+	// check for error
 	if err != nil {
 		return nil, nil, err
 	}
 
-	privateKeyBytes, err := x509.MarshalECPrivateKey(privateKey)
-
-	if err != nil {
-		return nil, nil, err
-	}
-
+	// obtain publicKey from privateKey and Marshal into bytes
 	publicKeyBytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
 
+	// Marshal privateKey into bytes
+	privateKeyBytes, err := x509.MarshalECPrivateKey(privateKey)
+
+	// check for error
 	if err != nil {
 		return nil, nil, err
 	}
@@ -43,16 +44,22 @@ func (e *curveP256) GenerateKeyPair() ([]byte, []byte, error) {
 // Sign signs the message with privateKey and returns a signature. It will
 // return nil if error occurs
 func (e *curveP256) Sign(privateKeyBytes, message []byte) []byte {
+	// get privateKey object from privateKeyBytes
 	privateKey, err := x509.ParseECPrivateKey(privateKeyBytes)
+
+	// check for error
 	if err != nil {
 		return nil
 	}
 
+	// sign message with privateKey
 	r, s, err := ecdsa.Sign(rand.Reader, privateKey, message)
+
 	if err != nil {
 		return nil
 	}
 
+	// obtain signature from r, s of type *big.Int
 	signature := r.Bytes()
 	signature = append(signature, s.Bytes()...)
 
