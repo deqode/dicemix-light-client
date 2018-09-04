85fb01e02c4a86ec349002d6c59835f22370b7c0
Manjeet Thadani <mthandani@grepruby.com>
2018-09-04 15:43:18 +0530

    Added helper function

--- dc/helper.go
+++ dc/helper.go
@@ -7,6 +7,36 @@ import (
 	"github.com/shomali11/util/xhashes"
 )
 
+func obtainSlots(state *utils.State, totalMsgsCount uint32) ([]int, bool) {
+	var i, j uint32
+	slots := make([]int, state.MyMsgCount)
+	ok := true
+
+	// initialize slots
+	for i := range slots {
+		slots[i] = -1
+	}
+
+	// Run an ordinary DC-net with slot reservations
+	for j = 0; j < state.MyMsgCount; j++ {
+		index, count := -1, 0
+		for i = 0; i < totalMsgsCount; i++ {
+			if state.AllMsgHashes[i] == reduce(state.MyMessagesHash[j]) {
+				index, count = int(i), int(count+1)
+			}
+		}
+
+		// if there is exactly one i
+		// with all_msg_hashes[i] = my_msg_hashes[j] then
+		if count == 1 {
+			slots[j] = index
+		} else {
+			ok = false
+		}
+	}
+	return slots, ok
+}
+
 func shortHash(message string) uint64 {
 	// NOTE: after DC-EXP roots would contain hash reduced into field
 	// (as final result would be in field)
